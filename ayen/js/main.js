7b46a8f7531da7ec39cdbaaf3efd83ed36853bfa
your username <rehamweb@gmail.com>
2020-01-23 15:18:51 +0200

    home page

--- ayen/js/main.js
+++ ayen/js/main.js
@@ -19,7 +19,8 @@ function myFunction() {
   const form = document.getElementById("form");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const error = document.getElementById('error-alert')
+const error = document.getElementById('error-alert');
+
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
@@ -27,18 +28,17 @@ function validateEmail(email) {
 form.addEventListener("submit", e => {
   let errorMessage=['']
   e.preventDefault();
-  if (email.value === "" || email.value=== null || validateEmail(email) ) {
-    errorMessage.push("خطا في اسم المستخدم ")
-  }
-  if(password.value === ""){
-    errorMessage.push("او كلمة المرور")
-  }
-  if(errorMessage.length >0){
-    error.innerHTML = `<div class="alert">
-    <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> ${  errorMessage.join('')}
-    </div>
-    `
+  if (email.value === "" || email.value=== null || validateEmail(email)|| password.value === "") {
+    error.classList.add('alert');
+    error.innerHTML = `
+    <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> خطا في اسم المستخدم او كلمة المرور
     
+    `
+  }else{
+    error.classList.remove('alert');
+
+    error.innerHTML=" "
   }
+  
 });
 
