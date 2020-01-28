4ba2cddf69443cba6c748c4fe5d29f537076c607
your username <rehamweb@gmail.com>
2020-01-28 13:32:29 +0200

    edit pages

--- ayen/js/main.js
+++ ayen/js/main.js
@@ -27,30 +27,4 @@ $(function() {
   });
 });
 
-/*validation form */
 
-//   const form = document.getElementById("form");
-// const email = document.getElementById("email");
-// const password = document.getElementById("password");
-// const error = document.getElementById('error-alert');
-
-// function validateEmail(email) {
-//   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(email);
-// }
-// form.addEventListener("submit", e => {
-//   let errorMessage=['']
-//   e.preventDefault();
-//   if (email.value === "" || email.value=== null || validateEmail(email)|| password.value === "") {
-//     error.classList.add('alert');
-//     error.innerHTML = `
-//     <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> خطا في اسم المستخدم او كلمة المرور
-
-//     `
-//   }else{
-//     error.classList.remove('alert');
-
-//     error.innerHTML=" "
-//   }
-
-// });
